26a259f66492c7d668219c3587c284c24b909381
ews4 <ews4@aber.ac.uk>
2022-02-14 18:42:22 +0000

    Modularised program further + added .gitignore Added a .gitignore file to stop large files clogging the repository. Imported 'datetime' package to use for default file name. Split the program into classes for the program and the algorithms. Added argument for a custom file name & changed the shorthand argument for "pathOut". Added check for output path to create folder if it doesn't exist.

--- videoToFrames.py
+++ videoToFrames.py
@@ -1,35 +1,78 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import argparse
 import os
+from datetime import datetime
 
 
-def find_best(path_in, path_out="Output/", num_to_output=5):
-    frames = cv2.VideoCapture(path_in)
-    success, image = frames.read()
-    count = 0
+class Autophotographer:
+    def __init__(self, path_in, path_out, quantity, filename):
+        self.path_in = path_in
+        self.path_out = path_out
+        self.num_to_output = quantity
+        self.filename = filename
 
-    liked = {}
+        self.find_best()
 
-    while success:
-        # Try to read next frame and increase counter by 1
-        success, image = frames.read()
-        count += 1
+    def find_best(self):
+        video = cv.VideoCapture(self.path_in)
+        success, image = video.read()
+        count = 0
 
-    # print({v for k, v in sorted(liked.items(), key=lambda item: item[1])})
+        liked = []
 
-    print(f'Number of frames: {count}')
-    print(f'Saved {num_to_output} images [Not really]')
+        while success:
+            if len(liked) < self.num_to_output:
+                liked.append(count)
 
-    frames.release()
+            # Try to read next frame and increase counter by 1
+            success, image = video.read()
+            count += 1
+
+        print(f'Number of frames: {count}')
+
+        self.save_best(video, liked)
+        video.release()
+
+    def save_best(self, video, img_list):
+        for i in img_list:
+
+            video.set(cv.CAP_PROP_POS_FRAMES, i)
+            success, frame = video.read()
+
+            if success:
+                if not os.path.exists(self.path_out):
+                    os.mkdir(self.path_out)
+
+                path = rf'{self.path_out}{args.name}{i}.png'
+                cv.imwrite(path, frame)
+                print(f"Saved frame {i} to {path}")
+            else:
+                print("Unable to save frame!")
+                break
+
+
+# These algorithms take in an image and return a score based on the content of the image
+class Algorithms:
+    def __init__(self, image):
+        self.image = image
+
+    def average_brightness(self):
+        pass
+
+    def sharpness(self):
+        pass
 
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
 
     args.add_argument("-i", "--pathIn", help="Path to Video File", required=True)
-    args.add_argument("-e", "--pathOut", help="Path to output saved images to", default="Output/")
-    args.add_argument("-o", "--output", help="Number of images to output", default=5)
+    args.add_argument("-o", "--pathOut", help="Path to output saved images to", default=rf'{os.getcwd()}/Output/')
+    args.add_argument("-q", "--quantity", help="Number of images to output", default=5)
+    args.add_argument("-n", "--name", help="The name of the files output (before file number is appended)",
+                      default=datetime.now().strftime('%d-%m-%y_%H-%M-%S_'))
 
     args = args.parse_args()
-    find_best(args.pathIn, args.pathOut, args.output)
+    
+    program = Autophotographer(args.pathIn, args.pathOut, args.quantity, args.name)
