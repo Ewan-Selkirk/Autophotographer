0a20d2ddbbe772530ff8e23de09409bb20f28f19
Ewan-Selkirk <ewanselkirk@sky.com>
2022-02-15 15:34:03 +0000

    Very basic algorithm implementation Added support for a basic average brightness algorithm using numpy average. Made a few functions global as they didn't need to be attached to a class. Changed the way the program handles args (all args are passed to the Autophotographer initializer and then set as variables rather than a ridiculously long initializer). Added argument for verbose output (currently does nothing). Broke down getting images and saving images into separate functions. Added an 'export_data' function that allows for data to be written to a file for debugging/making datasets. Added 'requirements.txt' to make setting up the project easier.

--- videoToFrames.py
+++ videoToFrames.py
@@ -4,61 +4,107 @@ import argparse
 import os
 from datetime import datetime
 
+verbose = False
+
+
+# The current implementation takes the 2D array of 'good' images and
+# replaces the second element (originally the average score in this instance)
+# with the image array
+# TODO: Find a better way to do this
+def fetch_frames(video, img_list) -> []:
+    for i in img_list:
+        video.set(cv.CAP_PROP_POS_FRAMES, i[0])
+        success, frame = video.read()
+
+        if success:
+            i[1] = frame
+
+    return img_list
+
+
+def export_data(name, data):
+    f = open(name, "w")
+    for d in data:
+        f.write(f"{d}\n")
+
+    f.close()
+
+
+def as_percentage(value) -> float:
+    if not value < 0 and not value > 255:
+        if value < (256 / 2) - 1:
+            return value / ((256 / 2) - 1) * 100
+        else:
+            return (255 - value) / ((256 / 2) - 1) * 100
+    else:
+        raise ValueError("Value must be between 0-255")
+
 
 class Autophotographer:
-    def __init__(self, path_in, path_out, quantity, filename):
-        self.path_in = path_in
-        self.path_out = path_out
-        self.num_to_output = quantity
-        self.filename = filename
+    def __init__(self, args):
+        self.path_in = args.pathIn
+        self.path_out = args.pathOut
+        self.num_to_output = args.quantity
+        self.name = args.name
+        # self.config = args.config
 
         self.find_best()
 
     def find_best(self):
         video = cv.VideoCapture(self.path_in)
         success, image = video.read()
-        count = 0
+        f_count = 0
 
-        liked = []
+        # Create an empty 2D array to store the frame number and it's average brightness
+        liked = [[]]
 
         while success:
+            alg = Algorithms(image)
+            avg = alg.average_brightness()
+
+            # Initially fill the list with the first few frames
             if len(liked) < self.num_to_output:
-                liked.append(count)
+                liked.append([f_count, avg])
+                liked.sort(reverse=True)
+            else:
+                for c, v in liked:
+                    if avg > v:
+                        # Remove the last element of the array (the smallest value)
+                        liked.pop()
+                        liked.append([f_count, avg])
+                        liked.sort(reverse=True)  # In theory this shouldn't be needed
+                        break
 
             # Try to read next frame and increase counter by 1
             success, image = video.read()
-            count += 1
+            f_count += 1
 
-        print(f'Number of frames: {count}')
+        print(f'Number of frames: {f_count}')
 
-        self.save_best(video, liked)
+        self.save_best(fetch_frames(video, liked))
         video.release()
 
-    def save_best(self, video, img_list):
-        for i in img_list:
+    def save_best(self, images):
+        for c, i in images:
+            if not os.path.exists(self.path_out):
+                os.mkdir(self.path_out)
 
-            video.set(cv.CAP_PROP_POS_FRAMES, i)
-            success, frame = video.read()
+            path = rf'{self.path_out}{self.name}{c}.png'
+            cv.imwrite(path, i)
+            # print(f"Saved frame {i} to {path}")
 
-            if success:
-                if not os.path.exists(self.path_out):
-                    os.mkdir(self.path_out)
+    def parse_config(self, config):
+        pass
 
-                path = rf'{self.path_out}{args.name}{i}.png'
-                cv.imwrite(path, frame)
-                print(f"Saved frame {i} to {path}")
-            else:
-                print("Unable to save frame!")
-                break
+    # These algorithms take in an image and return a score based on the content of the image
 
 
-# These algorithms take in an image and return a score based on the content of the image
 class Algorithms:
     def __init__(self, image):
         self.image = image
 
-    def average_brightness(self):
-        pass
+    def average_brightness(self) -> float:
+        return np.average(self.image)
 
     def sharpness(self):
         pass
@@ -68,11 +114,12 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser()
 
     args.add_argument("-i", "--pathIn", help="Path to Video File", required=True)
+    # args.add_argument("-c", "--config", help="", required=True,
+    #                   choices=[], nargs="+")
     args.add_argument("-o", "--pathOut", help="Path to output saved images to", default=rf'{os.getcwd()}/Output/')
     args.add_argument("-q", "--quantity", help="Number of images to output", default=5)
     args.add_argument("-n", "--name", help="The name of the files output (before file number is appended)",
                       default=datetime.now().strftime('%d-%m-%y_%H-%M-%S_'))
+    args.add_argument("-v", "--verbose", help="Prints messages to the console window", action="store_true")
 
-    args = args.parse_args()
-    
-    program = Autophotographer(args.pathIn, args.pathOut, args.quantity, args.name)
+    program = Autophotographer(args.parse_args())
