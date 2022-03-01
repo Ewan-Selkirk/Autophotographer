21ff81a42953fbe356612242641ef610c6b46601
ewan-selkirk <ewanselkirk@sky.com>
2022-03-01 20:10:21 +0000

    Big refactor for upcoming changes Changed the way input paths are handled, allowing for a folder of video files to be operated on at once. Added the Regular Expressions and math packages for certain functions. Changed 'as_percentage' to take the min and max as inputs in the event 0-255 is not the scale I want to work on. Added 'owner' variable to log function allowing a message to print where it came from (E.G. test.mp4: Detected 12 frames). Added 'get_array_vector' function for easily getting three values from an array in a much tidier way. Added more checks for if the filepath input exists. Added 'is_blurry' function for checking if the current frame is above a certain threshold and skipping the frame if not.

--- videoToFrames.py
+++ videoToFrames.py
@@ -2,7 +2,9 @@ import cv2 as cv
 import numpy as np
 import argparse
 import os
+import re
 from datetime import datetime
+import math
 
 verbose = False
 
@@ -30,86 +32,140 @@ def export_data(name, data):
     f.close()
 
 
-def as_percentage(value) -> float:
-    if value < 0 or value > 255:
-        raise ValueError("Value must be between 0-255")
+# Takes an input value and maps it to a user defined range,
+# return the percentage
+def as_percentage(value, r_min, r_max) -> float:
+    if value < r_min or value > r_max:
+        raise ValueError(f"Value must be between {r_min}-{r_max}")
 
-    if value <= 127:
-        return value / 127 * 100
+    if value <= r_max // 2:
+        return value / (r_max // 2) * 100
     else:
-        return 200 - (value / 127) * 100
+        return math.ceil(200 - (value / (r_max // 2)) * 100)
 
 
-def log(message):
+# Print messages to the console only if the '-v' argument is passed at the start
+# Prepends a timestamp to every message
+# Optional owner variable will print where the message is coming from (E.G. Which file is currently
+# being operated on)
+def log(message, owner='') -> None:
     if verbose:
-        print(f"[{datetime.now().strftime('%H:%M:%S')}]", message)
+        print(f"[{datetime.now().strftime('%H:%M:%S')}]", f"{owner + ': ' if owner != '' else ''}{message}")
+
+
+# Return a private function which returns 3 values from the provided array
+def get_array_vector(array) -> []:
+    return lambda a, b, c: [array[a], array[b], array[c]]
 
 
 class Autophotographer:
-    def __init__(self, args):
-        self.path_in = args.pathIn
-        self.path_out = args.pathOut
-        self.num_to_output = args.quantity
-        self.filename = args.name or str(f"{datetime.now().strftime('%d-%m-%y_%H-%M-%S_')}"
-                                         f"{args.pathIn.split('/')[-1][:-4]}_")
-        # self.config = args.config
+    def __init__(self, p_args):
+        # Create an array to store our loaded video files in
+        self.video = []
+
+        # Set our console arguments as variables
+        self.path_in = p_args.pathIn
+        self.path_out = p_args.pathOut
+        self.num_to_output = p_args.quantity
+        self.filename = p_args.name or str(f"{datetime.now().strftime('%d-%m-%y_%H-%M-%S_')}"
+                                           f"{p_args.pathIn.split('/')[-1][:-4]}_")
+        self.config = p_args.config
 
         # Set the global verbose variable if the verbose argument is passed
         global verbose
-        verbose = args.verbose
+        verbose = p_args.verbose
+
+        self.start()
+
+    def start(self):
+        if not os.path.exists(self.path_in):
+            raise FileNotFoundError("")
+
+        # Check if the input path ends with a file extension using Regular Expression
+        if re.search(r"\.[a-z]{3}", self.path_in):
+            try:
+                f = self.path_in.split('/')[-1]
+                self.video.append([f, cv.VideoCapture(self.path_in)])
+                log(f"Using file: {f} to operate on!")
+            except cv.error as err:
+                raise Exception(err)
+        else:
+            for filename in os.listdir(self.path_in):
+                self.video.append([filename, cv.VideoCapture(os.path.join(self.path_in, filename))])
+
+            log(f"Found {len(self.video)} {'files' if len(self.video) > 1 else 'file'} to operate on!")
 
         self.find_best()
 
     def find_best(self):
         t_start = datetime.now()
-        video = cv.VideoCapture(self.path_in)
-        success, image = video.read()
-        f_count = 0
 
-        # Create an empty 2D array to store the frame number, and it's average brightness
-        liked = [[]]
-
-        while success:
-            alg = Algorithms(image)
-            avg = alg.average_brightness()
-
-            # Initially fill the list with the first few frames
-            if len(liked) < self.num_to_output:
-                liked.append([f_count, avg])
-                liked.sort(reverse=True)
-            else:
-                for c, v in liked:
-                    if avg > v:
-                        # Remove the last element of the array (the smallest value)
-                        liked.pop()
-                        liked.append([f_count, avg])
-                        liked.sort(reverse=True)  # In theory this shouldn't be needed
-                        break
-
-            # Try to read next frame and increase counter by 1
+        for filename, video in self.video:
             success, image = video.read()
-            f_count += 1
+            f_count = 0
 
-        log(f'Number of frames: {f_count}')
+            # Create an empty 2D array to store the frame number, and it's average brightness
+            liked = []
 
-        self.save_best(fetch_frames(video, liked))
-        video.release()
+            if not success:
+                raise FileNotFoundError("Unable to read frames from input video file!")
+
+            while success:
+                # Create an instance of the Algorithms class with the current frame as the input
+                alg = Algorithms(image)
+
+                # If the video frame is blurry:
+                if alg.is_blurry(100):
+                    # Skip to the next frame
+                    success, image = video.read()
+                    break
+                else:
+                    s_img = alg.rot_split()
+                    data = [self.parse_config(alg)]
+
+                    # Initially fill the list with the first few frames
+                    if len(liked) < self.num_to_output:
+                        liked.append([f_count, np.average(data)])
+                        liked.sort(key=lambda x: x[1], reverse=True)
+                    else:
+                        for c, v in liked:
+                            if np.average(data) > v:
+                                # Remove the last element of the array (the smallest value)
+                                liked.pop()
+                                liked.append([f_count, np.average(data)])
+                                # Reverse sort the array by the value of the second
+                                liked.sort(key=lambda x: x[1], reverse=True)
+                                break
+
+                # Try to read next frame and increase counter by 1
+                success, image = video.read()
+                f_count += 1
+
+            log(f'Number of frames: {f_count}', owner=filename)
+
+            self.save_best(fetch_frames(video, liked), filename)
+            video.release()
+
         t_end = datetime.now()
         t_diff = (t_end - t_start).seconds
-        log(f"Operation took {str(divmod(t_diff, 60)[0]) + ' minutes and ' if divmod(t_diff, 60)[0] > 0 else ''}"
+        log(f"Operation took {str(divmod(t_diff, 60)[0]) + 'minutes and ' if divmod(t_diff, 60)[0] > 0 else ''}"
             f"{divmod(t_diff, 60)[1]} seconds to complete")
 
-    def save_best(self, images):
+    def save_best(self, images, file):
         for c, i in images:
             if not os.path.exists(self.path_out):
                 os.mkdir(self.path_out)
 
             path = rf'{self.path_out}{self.filename}{c}.png'
             cv.imwrite(path, i)
-        log(f"Saved {len(images)} images to {self.path_out}")
+        log(f"Saved {len(images)} images to {self.path_out}", owner=file)
 
-    def parse_config(self, config):
-        pass
+    def parse_config(self, algorithm_obj):
+        for c in self.config:
+            if c == "brightness":
+                return algorithm_obj.average_brightness()
+            elif c == "sharpness":
+                pass
 
     # These algorithms take in an image and return a score based on the content of the image
 
@@ -130,21 +186,27 @@ class Algorithms:
         ]
 
     def average_brightness(self) -> float:
-        return as_percentage(np.average(self.image))
+        return as_percentage(np.average(self.image), 0, 255)
 
     def sharpness(self):
         pass
+
+    def is_blurry(self, threshold) -> bool:
+        return cv.Laplacian(self.image, cv.CV_64F).var() < threshold
 
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
 
-    args.add_argument("-i", "--pathIn", help="Path to Video File", required=True)
-    # args.add_argument("-c", "--config", help="", required=True,
-    #                   choices=[], nargs="+")
-    args.add_argument("-o", "--pathOut", help="Path to output saved images to", default=rf'{os.getcwd()}/Output/')
+    args.add_argument("-i", "--pathIn", help="Path to either a directory of video files, or a specific video file.",
+                      required=True)
+    args.add_argument("-c", "--config", help="List of operations to perform on the input video file(s)", required=True,
+                      choices=["brightness", "sharpness"], nargs="+")
+    args.add_argument("-o", "--pathOut", help="Path to output saved images to (will default to an 'Output' folder in "
+                                              "the current working directory)", default=rf'{os.getcwd()}/Output/')
     args.add_argument("-q", "--quantity", help="Number of images to output", default=5)
-    args.add_argument("-n", "--name", help="The name of the files output (before file number is appended)")
+    args.add_argument("-n", "--name", help="The name of the files output (will default to the date and time, and the "
+                                           "original filename if left out)")
     args.add_argument("-v", "--verbose", help="Prints messages to the console window", action="store_true")
 
     program = Autophotographer(args.parse_args())
