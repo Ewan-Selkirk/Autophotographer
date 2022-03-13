853ce50e7cd81e484e327396f1a75d6a511c8aa4
ewan-selkirk <ewanselkirk@sky.com>
2022-03-13 09:24:43 +0000

    Big refactor 2: Electric Boogaloo It works, but badly Imported the 'enum' package and added an 'Array_Vector' enum for quickly accessing the specific rows and columns of a 3x3 matrix. Updated 'log' function to re-use the same 'parent' value unless changed/set to None. Changed 'get_array_vector' function to work on arrays of any size (as the target array allows). Changed method of detecting file extensions as I somehow forgot 'mp4' has a number in it... Created an 'ImageCapture' class that closely resembles OpenCVs 'VideoCapture' class to be able to load images in the same way we load videos. Changed method of choosing the best image to scoring multiple categories then using the value to determine the best image. The current implementation is very crude. Changed 'parse_config' function to use reflection to call methods on an object instead of using many if statements. Created a 'Rule_of_Thirds' class and moved splitting images into it. Added base for 'color' algorithm and added an alias for 'colour'. Changed 'is_blurry' threshold variable to an optional variable since I don't know how to use parameters when using reflection (Will probably change to hard-coded later on).

--- videoToFrames.py
+++ videoToFrames.py
@@ -3,10 +3,26 @@ import numpy as np
 import argparse
 import os
 import re
+import enum
+
 from datetime import datetime
-import math
 
 verbose = False
+owner = None
+
+
+# An Enum for easily accessing the row and columns we need from a 3x3 grid
+# (Use enum.Enum to avoid an `Enum` variable from being created)
+class Array_Vector(enum.Enum):
+    TOP = (0, 1, 2)
+    LEFT = (0, 3, 6)
+    BOTTOM = (6, 7, 8)
+    RIGHT = (2, 5, 8)
+    # "You can't half a third!" blah blah blah fight me
+    TOP_HALF = (0, 1, 2, 3, 4, 5)
+    BOTTOM_HALF = (3, 4, 5, 6, 7, 8)
+    LEFT_HALF = (0, 1, 3, 4, 6, 7)
+    RIGHT_HALF = (1, 2, 4, 5, 7, 8)
 
 
 # The current implementation takes the 2D array of 'good' images and
@@ -14,12 +30,12 @@ verbose = False
 # with the image array
 # TODO: Find a better way to do this
 def fetch_frames(video, img_list) -> []:
-    for i in img_list:
-        video.set(cv.CAP_PROP_POS_FRAMES, i[0])
+    for v in range(len(img_list)):
+        video.set(cv.CAP_PROP_POS_FRAMES, img_list[v][0])
         success, frame = video.read()
 
         if success:
-            i[1] = frame
+            img_list[v][1] = frame
 
     return img_list
 
@@ -41,21 +57,25 @@ def as_percentage(value, r_min, r_max) -> float:
     if value <= r_max // 2:
         return value / (r_max // 2) * 100
     else:
-        return math.ceil(200 - (value / (r_max // 2)) * 100)
+        return round(200 - (value / (r_max // 2)) * 100)
 
 
 # Print messages to the console only if the '-v' argument is passed at the start
 # Prepends a timestamp to every message
-# Optional owner variable will print where the message is coming from (E.G. Which file is currently
+# Optional `parent` variable will print where the message is coming from (E.G. Which file is currently
 # being operated on)
-def log(message, owner='') -> None:
+# (The owner variable will retain its value until it is changed or reset to nothing)
+def log(message, parent=None) -> None:
     if verbose:
-        print(f"[{datetime.now().strftime('%H:%M:%S')}]", f"{owner + ': ' if owner != '' else ''}{message}")
+        global owner
+        if parent is not None and parent != owner:
+            owner = parent
+        print(f"[{datetime.now().strftime('%H:%M:%S')}]", f"{owner + ': ' if owner is not None else ''}{message}")
 
 
 # Return a private function which returns 3 values from the provided array
 def get_array_vector(array) -> []:
-    return lambda a, b, c: [array[a], array[b], array[c]]
+    return lambda t: [array[t[i]] for i in range(len(t))] if type(t) is (tuple or list) else ValueError("")
 
 
 class Autophotographer:
@@ -82,18 +102,29 @@ class Autophotographer:
             raise FileNotFoundError("")
 
         # Check if the input path ends with a file extension using Regular Expression
-        if re.search(r"\.[a-z]{3}", self.path_in):
+        if re.search(r"\.\w{3}", self.path_in):
             try:
-                f = self.path_in.split('/')[-1]
-                self.video.append([f, cv.VideoCapture(self.path_in)])
-                log(f"Using file: {f} to operate on!")
+                filename = self.path_in.split('/')[-1]
+                self.video.append([filename, cv.VideoCapture(self.path_in)])
+                log(f"Using file: {filename} to operate on!")
             except cv.error as err:
                 raise Exception(err)
         else:
-            for filename in os.listdir(self.path_in):
-                self.video.append([filename, cv.VideoCapture(os.path.join(self.path_in, filename))])
+            # Create an ImageCapture object for any image files found in the directory
+            images = ImageCapture(None)
 
-            log(f"Found {len(self.video)} {'files' if len(self.video) > 1 else 'file'} to operate on!")
+            for filename in os.listdir(self.path_in):
+                if filename.endswith((".png", ".jpg")):
+
+                    if images.get(cv.CAP_PROP_FRAME_COUNT) != len([x for x in os.listdir(self.path_in)
+                                                                   if re.search(r"\.\w{3}", x)]):
+                        images.open([os.path.join(self.path_in, x) for x in os.listdir(self.path_in)
+                                     if re.search(r"\.\w{3}", x)])
+                        self.video.append([self.path_in.split("/")[-2], images])
+                else:
+                    self.video.append([filename, cv.VideoCapture(os.path.join(self.path_in, filename))])
+
+            log(f"Found {len(self.video)} {'files' if len(self.video) > 1 else 'file'} to operate on!", parent=None)
 
         self.find_best()
 
@@ -104,46 +135,37 @@ class Autophotographer:
             success, image = video.read()
             f_count = 0
 
-            # Create an empty 2D array to store the frame number, and it's average brightness
-            liked = []
-
             if not success:
                 raise FileNotFoundError("Unable to read frames from input video file!")
 
+            # For every frame in the video, create an element for storing the scores
+            # used to rate images
+            score = {f: {"blurry": None, "thirdsy": None, c: None}
+                     for f in range(int(video.get(cv.CAP_PROP_FRAME_COUNT)))
+                     for c in self.config}
+
             while success:
-                # Create an instance of the Algorithms class with the current frame as the input
-                alg = Algorithms(image)
+                log(f"Working on frame {f_count}", parent=filename)
+                img_rot = Rule_of_Thirds(image)
 
-                # If the video frame is blurry:
-                if alg.is_blurry(100):
-                    # Skip to the next frame
-                    success, image = video.read()
-                    break
-                else:
-                    s_img = alg.rot_split()
-                    data = [self.parse_config(alg)]
+                score[f_count]["blurry"] = np.sum([img_rot.run_algorithm("is_blurry")]) / 9 * 100
+                score[f_count]["thirdsy"] = np.std([[*img_rot.calc_diff(self.config)[c].values()]
+                                                   for c in self.config])
 
-                    # Initially fill the list with the first few frames
-                    if len(liked) < self.num_to_output:
-                        liked.append([f_count, np.average(data)])
-                        liked.sort(key=lambda x: x[1], reverse=True)
-                    else:
-                        for c, v in liked:
-                            if np.average(data) > v:
-                                # Remove the last element of the array (the smallest value)
-                                liked.pop()
-                                liked.append([f_count, np.average(data)])
-                                # Reverse sort the array by the value of the second
-                                liked.sort(key=lambda x: x[1], reverse=True)
-                                break
+                for c in self.config:
+                    score[f_count][c] = np.mean(img_rot.run_algorithm(c))
 
                 # Try to read next frame and increase counter by 1
                 success, image = video.read()
                 f_count += 1
 
-            log(f'Number of frames: {f_count}', owner=filename)
+            data = []
+            for k, v in score.items():
+                data.append([k, np.std([*v.values()])])
 
-            self.save_best(fetch_frames(video, liked), filename)
+            data.sort(key=lambda x: x[1], reverse=True)
+            self.save_best(fetch_frames(video, data[:self.num_to_output]))
+
             video.release()
 
         t_end = datetime.now()
@@ -151,48 +173,144 @@ class Autophotographer:
         log(f"Operation took {str(divmod(t_diff, 60)[0]) + 'minutes and ' if divmod(t_diff, 60)[0] > 0 else ''}"
             f"{divmod(t_diff, 60)[1]} seconds to complete")
 
-    def save_best(self, images, file):
+    def save_best(self, images):
         for c, i in images:
             if not os.path.exists(self.path_out):
                 os.mkdir(self.path_out)
 
-            path = rf'{self.path_out}{self.filename}{c}.png'
+            path = os.path.join(self.path_out, self.filename + str(c) + '.png')
             cv.imwrite(path, i)
-        log(f"Saved {len(images)} images to {self.path_out}", owner=file)
+        log(f"Saved {len(images)} images to {self.path_out}")
 
     def parse_config(self, algorithm_obj):
         for c in self.config:
-            if c == "brightness":
-                return algorithm_obj.average_brightness()
-            elif c == "sharpness":
-                pass
-
-    # These algorithms take in an image and return a score based on the content of the image
+            # Use reflection to get pointers to the algorithm functions*-
+            return algorithm_obj.__getattribute__(c)
 
 
+# These algorithms take in an image and return a score based on the content of the image
 class Algorithms:
     def __init__(self, image):
         self.image = image
 
-    def rot_split(self):
+    def brightness(self) -> float:
+        result = np.average(self.image)
+        return result
+
+    def color(self) -> float:
+        pass
+
+    def colour(self) -> float:
+        return self.color()
+
+    def sharpness(self):
+        pass
+
+    def is_blurry(self, threshold=200) -> bool:
+        return cv.Laplacian(self.image, cv.CV_64F).var() < threshold
+
+
+class Rule_of_Thirds:
+    def __init__(self, split_image):
+        self.image = split_image
+        self.__split_img = []
+
+        self.__split()
+
+    def __split(self):
+        # log("Splitting image into 9 chunks...")
+
         shape = self.image.shape
 
         # Method for splitting images into chunks based on this StackOverflow answer
         # https://stackoverflow.com/a/47581978
-        return [
+        # TODO: Fix issues with images that don't equally divide by 3
+        self.__split_img = [
             self.image[x: x + shape[0] // 3, y: y + shape[1] // 3]
             for x in range(0, shape[0], shape[0] // 3)
             for y in range(0, shape[1], shape[1] // 3)
         ]
 
-    def average_brightness(self) -> float:
-        return as_percentage(np.average(self.image), 0, 255)
+    def get_image_splits(self) -> []:
+        return self.__split_img
 
-    def sharpness(self):
-        pass
+    def run_algorithm(self, algorithm) -> []:
+        # Use reflection to call the method on a brand new `Algorithms` instance
+        return [Algorithms(s).__getattribute__(algorithm)() for s in self.__split_img]
 
-    def is_blurry(self, threshold) -> bool:
-        return cv.Laplacian(self.image, cv.CV_64F).var() < threshold
+    def setup_calc(self, config):
+        data = {c: {} for c in config}
+        vector = {c: get_array_vector(self.run_algorithm(c)) for c in config}
+
+        return data, vector
+
+    # def calc_mean(self, config):
+    #     data, vector = self.setup_calc(config)
+    #
+    #     for c in vector:
+    #         for e in Array_Vector:
+    #             data[c][e.name] = np.mean([vector[c](e.value)])
+    #
+    #     return data
+
+    def calc_diff(self, config: list[str]):
+        data, _ = self.setup_calc(config)
+
+        for c in config:
+            tmp = self.run_algorithm(c)
+
+            for v in Array_Vector:
+                data[c][v.name] = []
+
+                data[c][v.name].append(abs(tmp[v.value[0]] - tmp[v.value[1]]))
+                data[c][v.name].append(abs(tmp[v.value[0]] - tmp[v.value[2]]))
+                data[c][v.name].append(abs(tmp[v.value[1]] - tmp[v.value[2]]))
+
+                data[c][v.name] = np.sum(data[c][v.name])
+
+        return data
+
+
+class ImageCapture:
+    def __init__(self, filename):
+        self.stream = []
+        self.position = 0
+
+        self.open(filename)
+
+    def read(self) -> tuple[bool, np.ndarray or None]:
+        success = False
+        data = None
+
+        if self.position < len(self.stream):
+            success = True
+            data = self.stream[self.position]
+            self.position += 1
+
+        return success, data
+
+    def get(self, prop):
+        if prop is cv.CAP_PROP_FRAME_COUNT:
+            return len(self.stream)
+        elif prop is cv.CAP_PROP_POS_FRAMES:
+            return self.position
+        else:
+            pass
+
+    def open(self, filename):
+        if type(filename) is list:
+            for img in filename:
+                self.stream.append(cv.imread(img))
+        elif type(filename) is str:
+            if str(filename).endswith((".png", ".jpg")):
+                self.stream.append(cv.imread(filename))
+            else:
+                for f in os.listdir(filename):
+                    self.stream.append(cv.imread(os.path.join(filename, f)))
+
+    def release(self):
+        self.stream = []
+        self.position = 0
 
 
 if __name__ == "__main__":
@@ -201,7 +319,7 @@ if __name__ == "__main__":
     args.add_argument("-i", "--pathIn", help="Path to either a directory of video files, or a specific video file.",
                       required=True)
     args.add_argument("-c", "--config", help="List of operations to perform on the input video file(s)", required=True,
-                      choices=["brightness", "sharpness"], nargs="+")
+                      choices=["brightness", "sharpness", "color", "colour"], nargs="+")
     args.add_argument("-o", "--pathOut", help="Path to output saved images to (will default to an 'Output' folder in "
                                               "the current working directory)", default=rf'{os.getcwd()}/Output/')
     args.add_argument("-q", "--quantity", help="Number of images to output", default=5)
